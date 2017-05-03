f973222e36906bfad932c9ceb59a9afba14d54e1
ableocardo <ayushbharadwaj74@gmail.com>
2017-05-03 23:43:47 +0530

    FPS_SCRIPTS

---
+++ COMPORT.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class COMPORT : MonoBehaviour
+{
+      SerialPort sp= new SerialPort("COM6", 38400);
+    public static string s;
+
+
+
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+    void Update()
+    {
+        if (sp.IsOpen)
+        {
+            try
+            {
+                
+                sp.Write(s);
+            }
+            catch (System.Exception)
+            {
+                //return null;
+            }
+        }
+    }
+
+}
